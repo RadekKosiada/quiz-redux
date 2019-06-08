74c10781ada96d4d36d5e336e3d930a457d90368
RadekKosiada <kosiadar@gmail.com>
2019-06-08 17:05:47 +0200

    Created reducer

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+import { FETCH_QUESTIONS, SUBMIT_ANSWER } from 'actions';
+
+const initialState = {
+  questions: [],
+  submittedAnswer: {}
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_QUESTIONS: 
+    return {
+      ...state,
+      items: action.payload
+    }
+    default:
+      return state;
+  }
+}
